0dad32b63f2f569b9e9ddba2fe1eac789c53de87
tiif <pekyuan@gmail.com>
2023-10-14 22:10:21 +0800

    Fix test failed problem

--- src/test/java/seedu/address/model/person/LeadTest.java
+++ src/test/java/seedu/address/model/person/LeadTest.java
@@ -13,8 +13,8 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalClients.ALICE;
-import static seedu.address.testutil.TypicalClients.BOB;
+import static seedu.address.testutil.TypicalLeads.ALICE;
+import static seedu.address.testutil.TypicalLeads.BOB;
 
 import org.junit.jupiter.api.Test;
 
